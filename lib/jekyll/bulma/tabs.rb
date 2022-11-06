8e34900fdfe8510d67f86cd5c9e0c00028f7146a
Yoko Harada <yokolet@gmail.com>
2022-11-06 16:12:14 +0900

    just scaffolded. initial commit

---
+++ lib/jekyll/bulma/tabs.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "tabs/version"
+
+module Jekyll
+  module Bulma
+    module Tabs
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
